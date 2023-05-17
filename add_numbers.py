9825019f58c2a8ebab67f68d00d0aaf771226a92
amelialdrew <ad652@cam.ac.uk>
2023-05-17 09:45:10 +0100

    Add python script to sum numbers

---
+++ add_numbers.py
@@ -0,0 +1,11 @@
+# Ask the user for the first number
+num1 = input("Enter the first number: ")
+
+# Ask the user for the second number
+num2 = input("Enter the second number: ")
+
+# Add the two numbers together
+result = float(num1) + float(num2)
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", result)
