cd0d374875b133146f1f48386b4bbf42790318ee
amelialdrew <ad652@cam.ac.uk>
2023-07-18 15:12:21 +0100

    Add mpi ping pong example

---
+++ mpi_ping_pong.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <mpi.h>
+
+#define PING_PONG_LIMIT 10
+
+int main (int argc, char * argv[])
+{
+  int rank, size;
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD,&rank);
+  MPI_Comm_size( MPI_COMM_WORLD,&size );
+
+  int ping_pong_count = 0;
+  int partner_rank = (rank + 1) % 2;
+
+  while (ping_pong_count < PING_PONG_LIMIT) {
+
+    if (rank == ping_pong_count % 2) {
+        // Increment the ping pong count before you send it
+        ping_pong_count++;
+        MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0,
+                 MPI_COMM_WORLD);
+        printf("%d sent and incremented ping_pong_count "
+               "%d to %d\n", rank, ping_pong_count,
+               partner_rank);
+    } 
+    
+    else {
+        MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%d received ping_pong_count %d from %d\n",
+               rank, ping_pong_count, partner_rank);
+    }
+
+  }
+
+}
