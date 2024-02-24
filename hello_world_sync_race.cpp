6d07fa47c4330e3f20403b3274b426b33c610de3
amelialdrew <ad652@cam.ac.uk>
2024-02-24 21:25:16 +0000

    Update race example

--- hello_world_sync_race.cpp
+++ hello_world_sync_race.cpp
@@ -16,25 +16,26 @@ int main() {
   omp_set_num_threads(num_threads);
   int array[num_threads];
   int result = 0;
-  int my_calculated_variable;  
   int iterations = 1000;
 
 
 #pragma omp parallel
-{  
+{
+  int my_calculated_variable;    
   int ID = omp_get_thread_num();
   
   for (int i=ID; i<iterations; i+=num_threads) {
 
-    my_calculated_variable = i*i*i*i*i;
+    my_calculated_variable = i*i*i;
+
+    //#pragma omp critical    
     result += some_calc(my_calculated_variable);
     
   }
   
 }
 
- printf("%d \n", my_calculated_variable);
-   
+ printf("%d \n", result);     
 
  return 0;
   
