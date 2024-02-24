e4bc747e132809b52d946f09c956197469a42fa9
amelialdrew <ad652@cam.ac.uk>
2024-02-24 16:58:11 +0000

    Cleanup directory

--- JRF/Snippets/F77/HelloWorld.f
+++
@@ -1,5 +0,0 @@
-       PROGRAM main 
-c      Simple hello world program
-       WRITE(*,*) 'Hello World'
-       STOP
-       END
--- JRF/Snippets/GoogleTest/cmake/ClangTidy.cmake
+++
@@ -1,8 +0,0 @@
-function(ClangTidy target)
-    find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
-
-    set_target_properties(${target}
-        PROPERTIES CXX_CLANG_TIDY
-        "${CLANG-TIDY_PATH};-checks=clang-analyzer-*,bugprone-*,cppcoreguidelines-*,modernize-*,readability-*"
-    )
-endfunction()
