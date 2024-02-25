4ab617373afca8da5a734ebce45cf7a04e6d3de2
amelialdrew <ad652@cam.ac.uk>
2024-02-25 15:15:17 +0000

    Update Parallel for

--- numerical_integration_solution_parallelfor.cpp
+++ numerical_integration_solution_parallelfor.cpp
@@ -2,7 +2,6 @@
 #include <omp.h>
 
 int num_steps = 100000;
-#define NUM_THREADS 4
 double step;
 
 int main ()
@@ -13,10 +12,7 @@ int main ()
   double sum[num_steps] = {0.}; //We need to know the number of threads before runtime, so we can set the size of the array  
   step = 1.0 / num_steps;
 
-  omp_set_num_threads(NUM_THREADS);
-  int global_num_threads = omp_get_num_threads();  
-
-#pragma omp parallel for
+#pragma omp parallel for num_threads(4)
   for (i=0; i<num_steps; i++)
     {
       double x;      
