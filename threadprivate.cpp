8db3368e65bb31772007cf2fc5f81b838989b1a8
amelialdrew <ad652@cam.ac.uk>
2024-02-28 13:06:25 +0000

    Threadprivate example

---
+++ threadprivate.cpp
@@ -0,0 +1,40 @@
+//Adapted from https://hpc-tutorials.llnl.gov/openmp/threadprivate_directive/
+#include <stdio.h>
+#include <omp.h>
+ 
+int  a, b = 20, i, tid;
+float x;
+
+#pragma omp threadprivate(a, x)
+ 
+int main ()  {
+ 
+/* Explicitly turn off dynamic threads */
+  omp_set_dynamic(0); 
+
+  printf("1st Parallel Region:\n");
+  
+#pragma omp parallel private(b,tid)
+{
+  tid = omp_get_thread_num();
+  a = tid;
+  b = tid;
+  x = 1.1 * tid +1.0;
+  printf("Thread %d:   a,b,x= %d %d %f\n",tid,a,b,x);
+}  /* end of parallel section */
+ 
+  printf("************************************\n");
+  printf("Master thread doing serial work here\n");
+  printf("************************************\n");
+ 
+  printf("2nd Parallel Region:\n");
+  
+#pragma omp parallel private(tid)
+{
+  tid = omp_get_thread_num();
+  printf("Thread %d:   a,b,x= %d %d %f\n",tid,a,b,x);
+}  /* end of parallel section */
+
+ return 0;
+
+}
